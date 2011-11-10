ca94cae59b505ffd1caa12fa39572adf1b210b31
Deepak Kotur <dkotur@codeaurora.org>
2011-11-09 18:30:29 -0800

    vidc: Select board specific memory heap id.
    Allocates ION memory from the heap id specified
    in the board file.
    
    Change-Id: If701972a632fa336c6447e95b79484739593f1da
    Signed-off-by: Deepak Kotur <dkotur@codeaurora.org>
    
    Conflicts:
    
    	arch/arm/mach-msm/devices-8960.c

--- arch/arm/mach-msm/devices-msm8x60.c
+++ arch/arm/mach-msm/devices-msm8x60.c
@@ -2486,10 +2486,11 @@ struct msm_vidc_platform_data vidc_platform_data = {
 #ifdef CONFIG_MSM_BUS_SCALING
 	.vidc_bus_client_pdata = &vidc_bus_client_data,
 #endif
-	.memtype = MEMTYPE_SMI_KERNEL,
 #ifdef CONFIG_MSM_MULTIMEDIA_USE_ION
+	.memtype = ION_HEAP_SMI_ID,
 	.enable_ion = 1,
 #else
+	.memtype = MEMTYPE_SMI_KERNEL,
 	.enable_ion = 0,
 #endif
 	.disable_dmx = 0,
